4774568d71f664c612524e895e3376f6c608cf02
mikatan-mikan <a200401021236@gmail.com>
2025-03-28 15:08:11 +0900

    ch statusを自動設定するように変更

--- features/core/make_intent.py
+++ features/core/make_intent.py
@@ -4,5 +4,6 @@ def make_intent():
     intents = discord.Intents.default()
     intents.members = True  # メンバーの入退出イベントを有効化
     intents.message_content = True
+    intents.presences = True
     client = discord.Client(intents=intents)
     return client
