db423ca139331728ed9729e5cb4597cc71af4142
mikatan-mikan <a200401021236@gmail.com>
2025-04-11 18:17:32 +0900

    on_readyが複数回実行されないように変更

--- features/core/on_ready.py
+++ features/core/on_ready.py
@@ -3,10 +3,15 @@ import features.initialize.main
 import features
 from public.logging.logger import MainLogger
 
+is_ready = False
+
 def register(client: discord.Client, tree: discord.app_commands.CommandTree):
     logger = MainLogger()
     @client.event
     async def on_ready():
+        global is_ready
+        if is_ready: return
+        is_ready = True
         # 必要に応じてデータを初期化
         await features.initialize.main.run(client, tree)
         # ログ出力
